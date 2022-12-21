3db6fbdae654285fa52953f39cc23d1b671aecab
Spencer T. Parkin <Spencer.Parkin@L3T.com>
2022-12-21 14:35:37 -0700

    Start of triangle rasterizer.  Not finished or tested yet.

--- Source/Image.cpp
+++ Source/Image.cpp
@@ -1,5 +1,6 @@
 #include "Image.h"
 #include "Vertex.h"
+#include <math.h>
 
 using namespace Frumpy;
 
@@ -79,9 +80,114 @@ void Image::CalcImageMatrix(Matrix& imageMatrix) const
 
 void Image::RenderTriangle(const Vertex& vertexA, const Vertex& vertexB, const Vertex& vertexC, const PipelineMatrices& pipelineMatrices, Image& depthBuffer)
 {
-	// TODO: This is where the rubber meets the road.  How do we do it?  Converting from projection space to image space is easy, and
-	//       so is the process of filling in the triangle, one scan-line at a time.  The hard part is interpolating depth values and
-	//       other things.
+	struct Edge
+	{
+		const Vector* vertexA;
+		const Vector* vertexB;
+	};
 
+	Edge minEdges[2];
+	Edge maxEdges[2];
 
+	double minY = FRUMPY_MIN(FRUMPY_MIN(vertexA.imageSpacePoint.y, vertexB.imageSpacePoint.y), vertexC.imageSpacePoint.y);
+	double maxY = FRUMPY_MAX(FRUMPY_MAX(vertexA.imageSpacePoint.y, vertexB.imageSpacePoint.y), vertexC.imageSpacePoint.y);
+
+	double midY = 0.0;
+	
+	if (minY == vertexA.imageSpacePoint.y)
+	{
+		if (maxY == vertexB.imageSpacePoint.y)
+			midY = vertexC.imageSpacePoint.y;
+		else
+			midY = vertexB.imageSpacePoint.y;
+
+		minEdges[0].vertexA = &vertexA.imageSpacePoint;
+		minEdges[0].vertexB = &vertexB.imageSpacePoint;
+		minEdges[1].vertexA = &vertexA.imageSpacePoint;
+		minEdges[1].vertexB = &vertexC.imageSpacePoint;
+	}
+	else if(minY == vertexB.imageSpacePoint.y)
+	{
+		if (maxY == vertexA.imageSpacePoint.y)
+			midY = vertexC.imageSpacePoint.y;
+		else
+			midY = vertexA.imageSpacePoint.y;
+
+		minEdges[0].vertexA = &vertexB.imageSpacePoint;
+		minEdges[0].vertexB = &vertexA.imageSpacePoint;
+		minEdges[1].vertexA = &vertexB.imageSpacePoint;
+		minEdges[1].vertexB = &vertexC.imageSpacePoint;
+	}
+	else
+	{
+		if (maxY == vertexA.imageSpacePoint.y)
+			midY = vertexB.imageSpacePoint.y;
+		else
+			midY = vertexA.imageSpacePoint.y;
+
+		minEdges[0].vertexA = &vertexC.imageSpacePoint;
+		minEdges[0].vertexB = &vertexA.imageSpacePoint;
+		minEdges[1].vertexA = &vertexC.imageSpacePoint;
+		minEdges[1].vertexB = &vertexB.imageSpacePoint;
+	}
+
+	if (maxY == vertexA.imageSpacePoint.y)
+	{
+		maxEdges[0].vertexA = &vertexA.imageSpacePoint;
+		maxEdges[0].vertexB = &vertexB.imageSpacePoint;
+		maxEdges[1].vertexA = &vertexA.imageSpacePoint;
+		maxEdges[1].vertexB = &vertexC.imageSpacePoint;
+	}
+	else if (maxY == vertexB.imageSpacePoint.y)
+	{
+		maxEdges[0].vertexA = &vertexB.imageSpacePoint;
+		maxEdges[0].vertexB = &vertexA.imageSpacePoint;
+		maxEdges[1].vertexA = &vertexB.imageSpacePoint;
+		maxEdges[1].vertexB = &vertexC.imageSpacePoint;
+	}
+	else
+	{
+		maxEdges[0].vertexA = &vertexC.imageSpacePoint;
+		maxEdges[0].vertexB = &vertexA.imageSpacePoint;
+		maxEdges[1].vertexA = &vertexC.imageSpacePoint;
+		maxEdges[1].vertexB = &vertexB.imageSpacePoint;
+	}
+
+	int minRow = int(floor(minY));
+	int maxRow = int(ceil(maxY));
+	int midRow = int(round(midY));
+
+	minRow = FRUMPY_MAX(minRow, 0);
+	maxRow = FRUMPY_MIN(maxRow, signed(this->height));
+
+	for (int row = minRow; row <= maxRow; row++)
+	{
+		Edge* edges = (row < midRow) ? minEdges : maxEdges;
+		
+		double y = double(row);
+		
+		double t0 = (y - edges[0].vertexB->y) / (edges[0].vertexA->y - edges[0].vertexB->y);
+		double x0 = edges[0].vertexA->x * t0 + edges[0].vertexB->x * (1.0 - t0);
+		
+		double t1 = (y - edges[1].vertexB->y) / (edges[1].vertexA->y - edges[1].vertexB->y);
+		double x1 = edges[1].vertexA->x * t1 + edges[1].vertexB->x * (1.0 - t1);
+		
+		double minX = FRUMPY_MIN(x0, x1);
+		double maxX = FRUMPY_MAX(x0, x1);
+		
+		int minCol = int(floor(minX));
+		int maxCol = int(ceil(maxX));
+
+		minCol = FRUMPY_MAX(minCol, 0);
+		maxCol = FRUMPY_MIN(maxCol, signed(this->width));
+
+		for (int col = minCol; col <= maxCol; col++)
+		{
+			Location location{ unsigned(row), unsigned(col) };
+			Pixel* pixel = this->GetPixel(location);
+			// TODO: Fill in the scan-line here.  How do we interpolate the Z values?
+
+			//Vector triangleImagePoint(double(col), double(row), )
+		}
+	}
 }
